b208df875a60602b7ebb6982d51d10f03d3c676b
oroiteS <oroite@qq.com>
2026-06-30 14:32:06 +0800

    feat: 实现 main.dart 入口 + 底部导航 + 路由

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:workout_manager/screens/today_training_screen.dart';
+import 'package:workout_manager/screens/template_screen.dart';
+import 'package:workout_manager/screens/chart_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: WorkoutApp()));
+}
+
+class WorkoutApp extends StatelessWidget {
+  const WorkoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '训练记录',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const MainScreen(),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/chart') {
+          return MaterialPageRoute(
+            builder: (_) => const ChartScreen(),
+            settings: settings,
+          );
+        }
+        return null;
+      },
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final _screens = const [
+    TodayTrainingScreen(),
+    TemplateScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (i) => setState(() => _currentIndex = i),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.fitness_center_outlined),
+            selectedIcon: Icon(Icons.fitness_center),
+            label: '今日训练',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.calendar_month_outlined),
+            selectedIcon: Icon(Icons.calendar_month),
+            label: '周模板',
+          ),
+        ],
+      ),
+    );
+  }
+}
