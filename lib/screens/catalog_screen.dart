af784e7fa090f728e56eeb0a742c2b2a31956c72
oroiteS <oroite@qq.com>
2026-07-12 11:13:35 +0800

    chore: bump display version to v1.1.1 and finish catalog feature

--- lib/screens/catalog_screen.dart
+++ lib/screens/catalog_screen.dart
@@ -25,7 +25,7 @@ class CatalogScreen extends ConsumerWidget {
           ),
           const Padding(
             padding: EdgeInsets.only(right: 12),
-            child: Center(child: Text('v1.1.0', style: TextStyle(fontSize: 12, color: Colors.grey))),
+            child: Center(child: Text('v1.1.1', style: TextStyle(fontSize: 12, color: Colors.grey))),
           ),
         ],
       ),
